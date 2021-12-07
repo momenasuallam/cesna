4e604b6c33be4de289eda0a8836539ec7cf4967a
momenasuallam <59420500+momenasuallam@users.noreply.github.com>
2021-12-08 02:56:05 +0500

    pages done

--- js/app.js
+++ js/app.js
@@ -12,19 +12,44 @@ $(document).ready(function () {
     document.body.classList.toggle(className);
   }
 });
-// function validateFormlogin() {
-//     var x = document.forms["loginformm"]["emaillogin"].value;
-//     var y = document.forms["loginformm"]["passlogin"].value;
-//     if (x == "" && y == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-//     else{
-//  window.location.href = "user-dashboard.html";
-//     }
 
-// }
-//  $("#logimbtnid").click(function () {
-  
-//  });
+        var gallery = document.querySelector("#gallery");
+        var getVal = function (elem, style) {
+          return parseInt(
+            window.getComputedStyle(elem).getPropertyValue(style)
+          );
+        };
+        var getHeight = function (item) {
+          return item.querySelector(".content").getBoundingClientRect().height;
+        };
+        var resizeAll = function () {
+          var altura = getVal(gallery, "grid-auto-rows");
+          var gap = getVal(gallery, "grid-row-gap");
+          gallery.querySelectorAll(".gallery-item").forEach(function (item) {
+            var el = item;
+            el.style.gridRowEnd =
+              "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+          });
+        };
+        gallery.querySelectorAll("img").forEach(function (item) {
+          item.classList.add("byebye");
+          if (item.complete) {
+            console.log(item.src);
+          } else {
+            item.addEventListener("load", function () {
+              var altura = getVal(gallery, "grid-auto-rows");
+              var gap = getVal(gallery, "grid-row-gap");
+              var gitem = item.parentElement.parentElement;
+              gitem.style.gridRowEnd =
+                "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+              item.classList.remove("byebye");
+            });
+          }
+        });
+        window.addEventListener("resize", resizeAll);
+        gallery.querySelectorAll(".gallery-item").forEach(function (item) {
+          item.addEventListener("click", function () {
+            item.classList.toggle("full");
+          });
+        });
 
